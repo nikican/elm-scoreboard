642fefa6f3db049c2c76fa8c0c6cef8b841eb913
Nikica Novakovic <nikica@kortechs.me>
2017-06-22 00:05:51 +0200

    Create Subnit form

--- Main.elm
+++ Main.elm
@@ -1,8 +1,106 @@
 module Main exposing (..)
 
 import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (..)
+import String
 
 
-main : Html msg
+-- model
+
+
+type alias Model =
+    { players : List Player
+    , name : String
+    , playerId : Maybe Int
+    , plays : List Play
+    }
+
+
+type alias Player =
+    { id : Int
+    , name : String
+    , points : Int
+    }
+
+
+type alias Play =
+    { id : Int
+    , playerId : Int
+    , name : String
+    , points : Int
+    }
+
+
+initModel : Model
+initModel =
+    { players = []
+    , name = ""
+    , playerId = Nothing
+    , plays = []
+    }
+
+
+
+-- update
+
+
+type Msg
+    = Edit Player
+    | Score Player Int
+    | Input String
+    | Save
+    | Cancel
+    | DeletePlay Play
+
+
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        Input name ->
+            Debug.log "Input updated the model "
+                { model | name = name }
+
+        _ ->
+            model
+
+
+
+--view
+
+
+view : Model -> Html Msg
+view model =
+    div [ class "scoreboard" ]
+        [ h1 [] [ text "Score Keeper" ]
+        , playerForm model
+        , p [] [ text <| toString model ]
+        ]
+
+
+playerForm : Model -> Html Msg
+playerForm model =
+    Html.form [ onSubmit Save ]
+        [ input
+            [ type_ "text"
+            , placeholder "Add/Edit player.."
+            , onInput Input
+            , value model.name
+            ]
+            []
+        , button [ type_ "submit" ] [ text "Save" ]
+        , button [ type_ "button", onClick Cancel ] [ text "Cancel" ]
+        ]
+
+
+
+--uncomment if needed -- import Html.App as App
+
+
+main : Program Never Model Msg
 main =
-    text "Hellow Scorekeeper"
+    Html.beginnerProgram
+        { model = initModel
+        , view = view
+        , update = update
+        }
